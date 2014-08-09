8080a5ee80b0db75ff7dcd3aaefc79429e1f4955
Przemysław <Przemysaw@192.168.2.7>
2014-08-09 18:53:06 +0100

    BuildTrees changed to build trees for different combinations of number of players and board size values.
    ISSUE: High memory consumption!

--- src/main/java/BuildTrees.java
+++ src/main/java/BuildTrees.java
@@ -1,4 +1,5 @@
 
+import com.przmnwck.games.ballsengine.Engine;
 import decisiontrees.DecisionTreesBuilder;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -15,16 +16,27 @@ import java.util.logging.Logger;
  * @author Przemo
  */
 public class BuildTrees {
-    
+    static final int MIN_NUMBER_OF_PLAYERS=2;
     public static void main(String[] args){
-        DecisionTreesBuilder builder = new DecisionTreesBuilder(2, 3);
-        builder.buildTree();
-        try {
-            builder.saveTree(builder.getCurrentTree(), "D:/Trees", 3, 2);
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(BuildTrees.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(BuildTrees.class.getName()).log(Level.SEVERE, null, ex);
+        //build trees for different combinationsof number of players and field sies
+        int[] boardSizes = new int[]{3, 4, 5, 6, 7, 8};
+        int[] nop = new int[] {2, 3, 3, 4, 4, 4}; //maximum number of players
+        DecisionTreesBuilder builder;
+        for (int i = 0; i < boardSizes.length; i++) {
+            for (int p = MIN_NUMBER_OF_PLAYERS; p <= nop[i]; p++) {
+                builder = new DecisionTreesBuilder(p, boardSizes[i]);
+                System.out.println("DecisionTreeBuilder for np="+p+", bs="+boardSizes[i]+" ready.");
+                builder.buildTree();
+                System.out.println("Tree built.");
+                try {
+                    builder.saveTree(builder.getCurrentTree(), Engine.TREES_DIRECTORY, boardSizes[i], p);
+                    System.out.println("Tree created and saved for np="+p+", bs="+boardSizes[i]);
+                } catch (FileNotFoundException ex) {
+                    Logger.getLogger(BuildTrees.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IOException ex) {
+                    Logger.getLogger(BuildTrees.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
         }
     }
 }
